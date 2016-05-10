b85151f6c723fac3468d5f64f63a67830245f2ee
Jessica Tran Hur <jesshur824@gmail.com>
2016-05-10 10:48:58 -0500

    add directive vs. component as seen before. Instantly change nav bar based on authentication

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,6 +2,14 @@ import angular from 'angular';
 
 let componentsModule = angular.module('app.components', []);
 
+//components: meant to be standalone HTML elements
+//<list-errors></list-errors>
 import ListErrors from './list-errors.component';
 componentsModule.component('listErrors', ListErrors);
+
+//directives: used to invoke functionality on an existing HTML element via attribute
+//<div hide-this-element="true"> ... </div>
+import ShowAuthed from './show-authed.directive';
+componentsModule.directive('showAuthed', ShowAuthed);
+
 export default componentsModule;
