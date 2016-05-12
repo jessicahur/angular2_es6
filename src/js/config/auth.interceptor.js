c36f9626640f1fbbf407c213ba9fa047dc7527df
Jessica Tran Hur <jesshur824@gmail.com>
2016-05-12 08:12:38 -0500

    add JWT service and auth interceptor to modify request headers for specific urls

---
+++ src/js/config/auth.interceptor.js
@@ -0,0 +1,25 @@
+export default function authInterceptor(JWT, AppConstants, $window, $q) {
+  'ngInject';
+
+  return {
+    //automatically attach Authorization header
+    requesst: function (config) {
+      if (config.url.indexOf(AppConstants.api) === 0 && JWT.get()) {
+        config.headers.Authorization = 'Token ' + JWT.get();
+      }
+      return config;
+    },
+
+    //handle 401
+    responseError: function (rejection) {
+      if (rejection.status === 401) {
+        //clear any JWT token being stored
+        JWT.destroy();
+        //do a hard page refresh
+        $window.location.reload();
+      }
+      return $q.reject(rejection);
+    }
+
+  }
+}
--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,8 +1,12 @@
 export default class User {
-  constructor(AppConstants, $http) {
+  constructor(JWT, AppConstants, $http, $state, $q) {
     'ngInject';
+
+    this._JWT = JWT;
     this._AppConstants = AppConstants;
     this._$http = $http;
+    this._$state = $state;
+    this._$q = $q;
 
     //Object to store our use properties
     this.current = null;
@@ -18,11 +22,59 @@ export default class User {
       }
     }).then(
       (res) => {
+        //Set the JWT token
+        this._JWT.save(res.data.user.token);
+
+        //Store the user's info for easy lookup
         this.current = res.data.user;
 
         return res;
       }
     );
+  }
+
+  logout() {
+    this.current = null;
+    this._JWT.destroy();
+    //Do a hard reload of current state to ensure all data is flushed
+    this._$state.go(this._$state.$current, null, {reload: true});
+  }
+
+  verifyAuth() {
+    let deferred = this._$q.defer();
+
+    //check for JWT token first
+    if (!this._JWT.get()) {
+      deferred.resolve(false);
+      return deferred.promise;
+    }
+
+    //if there's a JWT & user is already set
+    if (this.current) {
+      deferred.resolve(true);
+    }
+    else {
+      //if current use isn't set, get it from the server.
+      //if server doesn't 401, set current user & resolve promise.
+      this._$http({
+        url: this._AppConstants.api + '/user',
+        method: 'GET'
+      }).then(
+        (res) => {
+          this.current = res.data.user;
+          deferred.resolve(true);
+        },
+        //if an error happens, that means the user's token was invalid
+        (err) => {
+          this._JWT.destroy();
+          deferred.resolve(false);
+        }
+        //Reject automatically handled by auth interceptor
+        //Will boot them to homepage
+        );
+    }
+
+    return deferred.promise;
   }
 
 }
